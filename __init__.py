22c4baa94819b3ed5f9e811f0ecefd06bd94ab8f
ryz <gitea@fake.local>
2022-01-04 17:41:54 +0300

    initial commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+__license__ = 'GPL 3'
+__copyright__ = '2012, Sergey Kuznetsov <clk824@gmail.com>'
+__docformat__ = 'restructuredtext en'
+
+from calibre.customize import StoreBase
+
+class FlibustaStore(StoreBase):
+    name = 'Флибуста'
+    description = _('Книжное братство')
+    actual_plugin = 'calibre_plugins.store_flibusta.flibusta:FlibustaStore'
